e18f4bd7db18743bb290fed276ff21a9e63e6922
AutomatedTester <dburns@mozilla.com>
2015-01-11 21:44:54 +0000

    Add test for posting content to the server

--- petulant/meme/tests.py
+++ petulant/meme/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+
+class MemeTests(TestCase):
+
+    def test_can_post_to_db(self):
+        response = json.loads(self.client.post('/', {'url':'https://foo.bar/baz.gif', 'keywords':'omg, this, is, great'}).content)
+        self.assertTrue(response['success'])
+
+
